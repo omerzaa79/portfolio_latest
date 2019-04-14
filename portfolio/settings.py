994244e0bebb323d6cb5eba3bc4a2f8ccea7c403
omerzaa79 <omerzaa79@gmail.com>
2019-04-14 16:26:03 +0500

    secured secret key and debug=False

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -8,10 +8,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k8av*yq@ceqdw*=8-9+!@tqcrl%vluz@=$bpb7%+8s6f!ppkbm'
+# SECRET_KEY = 'k8av*yq@ceqdw*=8-9+!@tqcrl%vluz@=$bpb7%+8s6f!ppkbm'
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'k8av*yq@ceqdw*=8-9+!@tqcrl%vluz@=$bpb7%+8s6f!ppkbm')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
